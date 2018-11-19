17351593237bab105ab9eb6b37ab21e39fbf7e22
1820480625 <1820480625@qq.com>
2018-11-19 15:09:39 +0800

    增加实体

---
+++ dubboxentity/src/main/java/com/szxs/entity/RecoveryDevice.java
@@ -0,0 +1,151 @@
+package com.szxs.entity;
+
+
+/**
+ * 机型
+ * @Title:recoveryDevice
+ * @Description: 
+ * @user: yiwu 
+ * @date:2018年7月7日 下午2:03:56
+ */
+public class RecoveryDevice {
+    private String uuid;
+
+    private String brandId;
+
+    private String deviceModel;
+
+    private String devicePictureId;
+
+    private Integer minPrice;
+
+    private Integer maxPrice;
+
+    private Long viewOrder;
+
+    private String createdBy;
+
+    private String creationDate;
+
+    private String lastUpdateBy;
+
+    private String lastUpdateDate;
+
+    private String extField1;
+
+    private String extField2;
+
+    private String memo;
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid == null ? null : uuid.trim();
+    }
+
+    public String getBrandId() {
+        return brandId;
+    }
+
+    public void setBrandId(String brandId) {
+        this.brandId = brandId == null ? null : brandId.trim();
+    }
+
+    public String getDeviceModel() {
+        return deviceModel;
+    }
+
+    public void setDeviceModel(String deviceModel) {
+        this.deviceModel = deviceModel == null ? null : deviceModel.trim();
+    }
+
+    public String getDevicePictureId() {
+        return devicePictureId;
+    }
+
+    public void setDevicePictureId(String devicePictureId) {
+        this.devicePictureId = devicePictureId == null ? null : devicePictureId.trim();
+    }
+
+    public Integer getMinPrice() {
+        return minPrice;
+    }
+
+    public void setMinPrice(Integer minPrice) {
+        this.minPrice = minPrice;
+    }
+
+    public Integer getMaxPrice() {
+        return maxPrice;
+    }
+
+    public void setMaxPrice(Integer maxPrice) {
+        this.maxPrice = maxPrice;
+    }
+
+    public Long getViewOrder() {
+        return viewOrder;
+    }
+
+    public void setViewOrder(Long viewOrder) {
+        this.viewOrder = viewOrder;
+    }
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy == null ? null : createdBy.trim();
+    }
+
+    public String getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(String creationDate) {
+        this.creationDate = creationDate == null ? null : creationDate.trim();
+    }
+
+    public String getLastUpdateBy() {
+        return lastUpdateBy;
+    }
+
+    public void setLastUpdateBy(String lastUpdateBy) {
+        this.lastUpdateBy = lastUpdateBy == null ? null : lastUpdateBy.trim();
+    }
+
+    public String getLastUpdateDate() {
+        return lastUpdateDate;
+    }
+
+    public void setLastUpdateDate(String lastUpdateDate) {
+        this.lastUpdateDate = lastUpdateDate == null ? null : lastUpdateDate.trim();
+    }
+
+    public String getExtField1() {
+        return extField1;
+    }
+
+    public void setExtField1(String extField1) {
+        this.extField1 = extField1 == null ? null : extField1.trim();
+    }
+
+    public String getExtField2() {
+        return extField2;
+    }
+
+    public void setExtField2(String extField2) {
+        this.extField2 = extField2 == null ? null : extField2.trim();
+    }
+
+    public String getMemo() {
+        return memo;
+    }
+
+    public void setMemo(String memo) {
+        this.memo = memo == null ? null : memo.trim();
+    }
+}
