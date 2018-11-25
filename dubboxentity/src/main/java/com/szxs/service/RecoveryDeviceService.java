24cf2adfee123fe48038c83e22ffcd6e3dc79cbc
1820480625 <1820480625@qq.com>
2018-11-25 14:14:58 +0800

    删除查询品牌方法

--- dubboxentity/src/main/java/com/szxs/service/RecoveryDeviceService.java
+++ dubboxentity/src/main/java/com/szxs/service/RecoveryDeviceService.java
@@ -60,14 +60,4 @@ public interface RecoveryDeviceService {
      * 根据ID查询对象
      */
     RecoveryDevice1 searchRecoveryDeviceById(RecoveryDevice1 recoveryDevice1);
-
-    /**
-     * @Auther:HuJinHua
-     * Description
-     * @User: Administrator
-     * @Date: 15:04 2018/11/24
-     * @param:  * @param null
-     * 查询所有回收设备
-     */
-    List<RecoveryDevice1> searchAllDevice();
 }
