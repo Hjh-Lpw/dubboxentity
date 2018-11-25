4446e5c5191f4dc2423ed5a9600b97857ab70d9a
1820480625 <1820480625@qq.com>
2018-11-25 16:29:39 +0800

    增加

--- dubboxentity/src/main/java/com/szxs/service/TRecoveryDeviceQuoteService.java
+++ dubboxentity/src/main/java/com/szxs/service/TRecoveryDeviceQuoteService.java
@@ -10,4 +10,45 @@ import com.szxs.entity.TRecoveryDeviceQuote;
  */
 public interface TRecoveryDeviceQuoteService {
     JqueryTable<TRecoveryDeviceQuote> selectTRecoveryDeviceQuote(int pageNo, int pageSize, int draw, TRecoveryDeviceQuote tRecoveryDeviceQuote);
+    /**
+     * @Auther:HuJinHua
+     * Description
+     * @User: Administrator
+     * @Date: 11:20 2018/11/22
+     * @param:  * @param null
+     * 根据ID删除回收报价单
+     */
+    Boolean deleteDeviceQuoteById(TRecoveryDeviceQuote tRecoveryDeviceQuote);
+
+    /**
+     * @Auther:HuJinHua
+     * Description
+     * @User: Administrator
+     * @Date: 11:20 2018/11/22
+     * @param:  * @param null
+     * 新增回收报价单
+     */
+    int insertRecoveryDeviceQuote(TRecoveryDeviceQuote tRecoveryDeviceQuote);
+
+
+    /**
+     * @Auther:HuJinHua
+     * Description
+     * @User: Administrator
+     * @Date: 11:21 2018/11/22
+     * @param:  * @param null
+     * 修改回收报价单
+     */
+    int updateTRecoveryDeviceQuoteByUUID(TRecoveryDeviceQuote tRecoveryDeviceQuote);
+
+
+    /**
+     * @Auther:HuJinHua
+     * Description
+     * @User: Administrator
+     * @Date: 11:22 2018/11/22
+     * @param:  * @param null
+     * 根据ID查询回收报价单
+     */
+    TRecoveryDeviceQuote searchTRecoveryDeviceQuoteById(TRecoveryDeviceQuote tRecoveryDeviceQuote);
 }
