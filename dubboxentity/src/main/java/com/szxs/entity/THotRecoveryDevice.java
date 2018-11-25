e1c2e9d795cef2204599dcef54364009f2f184a4
1820480625 <1820480625@qq.com>
2018-11-25 21:42:36 +0800

    增加热门手机

---
+++ dubboxentity/src/main/java/com/szxs/entity/THotRecoveryDevice.java
@@ -0,0 +1,117 @@
+package com.szxs.entity;
+
+
+public class THotRecoveryDevice {
+
+  private String uuid;
+  private String brandId;
+  private String deviceId;
+  private String viewOrder;
+  private String createdBy;
+  private String creationDate;
+  private String lastUpdateBy;
+  private String lastUpdateDate;
+  private String extField1;
+  private String extField2;
+  private String memo;
+
+
+  public String getUuid() {
+    return uuid;
+  }
+
+  public void setUuid(String uuid) {
+    this.uuid = uuid;
+  }
+
+
+  public String getBrandId() {
+    return brandId;
+  }
+
+  public void setBrandId(String brandId) {
+    this.brandId = brandId;
+  }
+
+
+  public String getDeviceId() {
+    return deviceId;
+  }
+
+  public void setDeviceId(String deviceId) {
+    this.deviceId = deviceId;
+  }
+
+
+  public String getViewOrder() {
+    return viewOrder;
+  }
+
+  public void setViewOrder(String viewOrder) {
+    this.viewOrder = viewOrder;
+  }
+
+
+  public String getCreatedBy() {
+    return createdBy;
+  }
+
+  public void setCreatedBy(String createdBy) {
+    this.createdBy = createdBy;
+  }
+
+
+  public String getCreationDate() {
+    return creationDate;
+  }
+
+  public void setCreationDate(String creationDate) {
+    this.creationDate = creationDate;
+  }
+
+
+  public String getLastUpdateBy() {
+    return lastUpdateBy;
+  }
+
+  public void setLastUpdateBy(String lastUpdateBy) {
+    this.lastUpdateBy = lastUpdateBy;
+  }
+
+
+  public String getLastUpdateDate() {
+    return lastUpdateDate;
+  }
+
+  public void setLastUpdateDate(String lastUpdateDate) {
+    this.lastUpdateDate = lastUpdateDate;
+  }
+
+
+  public String getExtField1() {
+    return extField1;
+  }
+
+  public void setExtField1(String extField1) {
+    this.extField1 = extField1;
+  }
+
+
+  public String getExtField2() {
+    return extField2;
+  }
+
+  public void setExtField2(String extField2) {
+    this.extField2 = extField2;
+  }
+
+
+  public String getMemo() {
+    return memo;
+  }
+
+  public void setMemo(String memo) {
+    this.memo = memo;
+  }
+
+}
---
+++ dubboxentity/src/main/java/com/szxs/service/THotRecoveryDeviceService.java
@@ -0,0 +1,21 @@
+package com.szxs.service;
+
+import com.szxs.entity.JqueryTable;
+import com.szxs.entity.THotRecoveryDevice;
+
+/**
+ * @Auther:HuJinHua
+ * @Descrioption:
+ * @Date:2018/11/25 21:40
+ */
+public interface THotRecoveryDeviceService {
+    /**
+     * @Auther:HuJinHua
+     * Description
+     * @User: Administrator
+     * @Date: 17:56 2018/11/19
+     * @param:  * @param null
+     * 查询所有热门手机分页
+     */
+    JqueryTable<THotRecoveryDevice> selectRecoveryDevice(int pageNo, int pageSize, int draw, THotRecoveryDevice tHotRecoveryDevice);
+}
