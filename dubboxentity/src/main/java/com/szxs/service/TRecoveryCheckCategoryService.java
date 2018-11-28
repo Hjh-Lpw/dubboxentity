dfae926bcadfd2ee5fe638a973f5b76ea46a635f
1820480625 <1820480625@qq.com>
2018-11-28 11:29:30 +0800

    增加searchTRecoveryCheckCategory

---
+++ dubboxentity/src/main/java/com/szxs/service/TRecoveryCheckCategoryService.java
@@ -0,0 +1,14 @@
+package com.szxs.service;
+
+import com.szxs.entity.TRecoveryCheckCategory;
+
+import java.util.List;
+
+/**
+ * @Auther:HuJinHua
+ * @Descrioption:
+ * @Date:2018/11/28 11:28
+ */
+public interface TRecoveryCheckCategoryService {
+    List<TRecoveryCheckCategory> searchTRecoveryCheckCategory(TRecoveryCheckCategory tRecoveryCheckCategory);
+}
